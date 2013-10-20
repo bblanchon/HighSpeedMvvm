22aba3352e5de704789d42ac63efd9b1271e7da6
Benoit Blanchon <benoit.blanchon@gmail.com>
2013-10-20 14:50:17 +0200

    Added Method4

---
+++ Method4/Model.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method4
+{
+    class Model
+    {
+        public Model()
+        {
+            Task.Run((Action)LongRunningBackgroundTask);
+        }
+
+        public double Progress { get; private set; }
+        public event Action<double> ProgressChanged;
+
+        void LongRunningBackgroundTask()
+        {
+            while( Progress < 100)
+            {
+                // Note about this example Model:
+                // You may need to have an even smaller increment on a fast computer
+                // otherwise Progress would reach 100 too quickly
+                Progress += 0.0000001; 
+
+                if (ProgressChanged != null)
+                    ProgressChanged(Progress);          
+            }
+        }
+    }
+}
---
+++ Method4/ViewModel.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Threading;
+
+namespace Method4
+{
+    class ViewModel : INotifyPropertyChanged, IRefresh
+    {
+        readonly Model model;
+        double progress;
+
+        public ViewModel()
+        {
+            model = new Model();
+        }
+              
+        public double Progress
+        {
+            get { return progress; }
+            set
+            {
+                if( progress == value ) return;
+                progress = value;
+                RaisePropertyChanged("Progress");
+            }
+        }
+
+        #region IRefresh
+
+        public void Refresh()
+        {
+            Progress = model.Progress;
+        }
+
+        #endregion
+
+        #region INotifyPropertyChanged
+
+        void RaisePropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }       
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        #endregion
+    }
+}
